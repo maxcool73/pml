278e2ab75a38c32351203d3d9c1a8857e766c48a
user <maxcool73@gmail.com>
2021-12-21 21:36:38 +0300

    sklearn start

--- adalinesgd/adalinesgd_t.py
+++ adalinesgd/adalinesgd_t.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from adalinesgd import AdalineSGD
 from commons import plot_decision_regions
 
-s = r'C:\Users\maxim.korolev\PycharmProjects\pml\perceptron\iris.data'
+# s = r'C:\Users\maxim.korolev\PycharmProjects\pml\perceptron\iris.data'
+s = '/home/user/Документы/Projects/Python/pml/perceptron/iris.data'
 print('URL: ', s)
 df = pd.read_csv(s, header=None, encoding='utf-8')
 y = df.iloc[0: 100, 4].values
@@ -17,7 +18,8 @@ X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
 
 ada_sgd = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
 ada_sgd.fit(X_std, y)
-plot_decision_regions(X_std, y, classifier=ada_sgd )
+# ada_sgd.partial_fit(X_std[0, :], y[0])
+plot_decision_regions(X_std, y, classifier=ada_sgd)
 plt.title('Adaline - стохастический градиентный спуск')
 plt.xlabel('длина чашелистика [стандартизированная]')
 plt.ylabel('длина лепестка [стандартизированная]')
@@ -26,7 +28,7 @@ plt.tight_layout()
 plt.show()
 
 plt.plot(range(1, len(ada_sgd.cost_) + 1), ada_sgd.cost_, marker='o')
-plt.xlabel ('Эпохи')
-plt.ylabel ('Усредненные издержки')
+plt.xlabel('Эпохи')
+plt.ylabel('Усредненные издержки')
 plt.tight_layout()
 plt.show()
