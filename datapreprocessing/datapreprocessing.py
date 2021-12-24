4a197b06de0044bf35d2cdea1a3ef2ffdeff2a2b
Maksim <maxcool73@mail.ru>
2021-12-24 18:32:19 +0300

    datapreprocessing2

--- datapreprocessing/datapreprocessing.py
+++ datapreprocessing/datapreprocessing.py
@@ -17,7 +17,31 @@ df.dropna(subset=['C'])
 
 from sklearn.impute import SimpleImputer
 import numpy as np
+
 imr = SimpleImputer(missing_values=np.nan, strategy='mean')
 imr = imr.fit(df.values)
-imputed_data = imr.transform(df .values)
+imputed_data = imr.transform(df.values)
 imputed_data
+df.fillna(df.mean())
+
+df = pd.DataFrame([
+    ['green', 'М', 10.1, 'class2'],
+    ['red', 'L', 13.5, 'class1'],
+    ['Ыuе', 'XL', 15.3, 'class2']])
+df.columns = ['color', 'size', 'price', 'classlabel']
+df
+size_mapping = {'XL': 3,
+                'L': 2,
+                'М': 1}
+df['size'] = df['size'].map(size_mapping)
+df
+inv_size_mapping = {v: k for k, v in size_mapping.items()}
+df['size'].map(inv_size_mapping)
+class_mapping = {label: idx for idx, label in
+                 enumerate(np.unique(df['classlabel']))}
+class_mapping
+df['classlabel'] = df['classlabel'].map(class_mapping)
+df
+inv_class_mapping = {v: k for k, v in class_mapping.items()}
+df['classlabel'] = df['classlabel'].map(inv_class_mapping)
+df
