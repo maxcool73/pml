95baa9c07bbdbf2e07dd11874fb12cebf7eeb74b
Maksim <maxcool73@mail.ru>
2022-01-27 17:47:41 +0300

    sentimentanalysis5

--- stuff.py
+++ stuff.py
@@ -6,4 +6,7 @@ data = '{"name": "John Smith", "hometown": {"name": "New York", "id": 123}}'
 # Parse JSON into an object with attributes corresponding to dict keys.
 x = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
 print(x.name, x.hometown.name, x.hometown.id)
-pass
+
+
+def test():
+    print("Two")
